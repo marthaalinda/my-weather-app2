f8b010a142dcd4f0e53c93c3b338a4827fe6940e
marthaalinda <94095027+marthaalinda@users.noreply.github.com>
2022-03-30 11:56:27 -0700

    Add a weather icon from API

--- src/index.js
+++ src/index.js
@@ -33,6 +33,8 @@ function displayweatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement=document.querySelector("#icon");
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
 }
 
 function searchCity(city) {
