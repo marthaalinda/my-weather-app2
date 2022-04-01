ca722b5ecab7e77262db84151643ebd9e9f88169
marthaalinda <94095027+marthaalinda@users.noreply.github.com>
2022-04-01 16:50:31 -0700

    Create forecast for the rest of the days

--- src/index.js
+++ src/index.js
@@ -21,9 +21,39 @@ function formatDate(date) {
   return `${days[dayIndex]},${hours}:${minutes}`;
 }
 
-function displayForecast(response){
- console.log(response.data)
+function formatDay(timestamp){
+  let date=new Date(timestamp*1000);
+  let day= date.getDay();
+  let days=["Sun", "Mon", "Tue", "Wed", "Thur","Fri", "Sat"];
+
+  return days[day]
 }
+
+function displayForecast(response){
+ let forecast=response.data.daily;
+
+ let forecastElement=document.querySelector("#forecast");
+
+ let forecastHTML= `<div class="row">`;
+ forecast.forEach(function(forecastDay, index){
+   if (index < 6){
+    forecastHTML=forecastHTML+`
+    <div class="col-2">
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36"/>
+     <div class="weather-forecast-temperatures">
+       <span class="weather-forecast-temperatures-max">${Math.round(forecastDay.temp.max)}&deg</span>
+       <span class="weather-forecast-temperatures-min">${Math.round(forecastDay.temp.min)}&deg</span>
+     </div>
+    </div>
+    `;
+   }
+  
+ })
+ forecastHTML=forecastHTML+ `</div>`;
+ forecastElement.innerHTML=forecastHTML;
+}
+
 
 function getForecast(coordinates){
   console.log(coordinates);
