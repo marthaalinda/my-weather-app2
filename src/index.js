3c713e3ef9ed6ad49f8261fefbd11f9df53ebf93
marthaalinda <94095027+marthaalinda@users.noreply.github.com>
2022-03-31 18:00:56 -0700

    Implet unit conversion

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function displayweatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+   celsiusTemperature
   );
   document.querySelector("#Humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#Wind").innerHTML = Math.round(
@@ -34,7 +34,8 @@ function displayweatherCondition(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
   let iconElement=document.querySelector("#icon");
-  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  celsiusTemperature=response.data.main.temp;
 }
 
 function searchCity(city) {
@@ -71,5 +72,30 @@ searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton= document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+function displayFahrenheitTemperature(event){
+event.preventDefault();
+let temperatureElement=document.querySelector("#temperature");
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature=(celsiusTemperature*9/5) +32;
+temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
+}
+
+let celsiusTemperature=null;
+
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement=document.querySelector("#temperature");
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  }
+
+  let fahrenheitLink=document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+  let celsiusLink=document.querySelector("#celsius-link");
+  fahrenheitLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Yamoussoukro");
